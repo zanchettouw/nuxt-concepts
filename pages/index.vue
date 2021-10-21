f776cca10078ab5269ee13579d2a9c0a59d9a507
Fernando Zanchetta <fernandozanca94@gmail.com>
2021-10-20 22:51:15 -0300

    Initiate project

---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <nuxt-link to="/product">Product</nuxt-link>
+    Welcome to nuxt.js!
+  </div>
+</template>
---
+++ pages/product.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>Hello! from product.</div>
+</template>
